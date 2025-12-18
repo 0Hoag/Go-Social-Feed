3d483963fd1de05b15a3248ac10a62b1b6c1624f
0Hoag <zzeross2005@gmail.com>
2025-12-18 13:45:34 +0700

    feat: remove run server in goroutine

--- internal/httpserver/httpserver.go
+++ internal/httpserver/httpserver.go
@@ -3,9 +3,6 @@ package httpserver
 import (
 	"context"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func (srv HTTPServer) Run() error {
@@ -14,16 +11,23 @@ func (srv HTTPServer) Run() error {
 		return err
 	}
 
-	ctx := context.Background()
-	go func() {
-		srv.gin.Run(fmt.Sprintf(":%d", srv.port))
-	}()
+	// ctx := context.Background()
+	// go func() {
+	// 	srv.gin.Run(fmt.Sprintf(":%d", srv.port))
+	// }()
 
-	srv.l.Infof(ctx, "Started server on :%d", srv.port)
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	srv.l.Info(ctx, <-ch)
-	srv.l.Info(ctx, "Stopping API server.")
+	// srv.l.Infof(ctx, "Started server on :%d", srv.port)
+	// ch := make(chan os.Signal, 1)
+	// signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	// srv.l.Info(ctx, <-ch)
+	// srv.l.Info(ctx, "Stopping API server.")
+
+	addr := fmt.Sprintf(":%d", srv.port)
+	srv.l.Infof(context.Background(), "Starting HTTP server on %s", addr)
+
+	if err := srv.gin.Run(addr); err != nil {
+		return err
+	}
 
 	return nil
 }
