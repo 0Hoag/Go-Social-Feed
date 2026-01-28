ca241ef81ac8013dbc8348e07e382f6adb9ec067
Zeross <zeross@MacBook-Pro-2.local>
2026-01-28 17:41:31 +0700

    feat: refactor model role and permission

--- internal/models/user.go
+++ internal/models/user.go
@@ -7,15 +7,45 @@ import (
 )
 
 type User struct {
-	ID           primitive.ObjectID `bson:"_id,omitempty"`
-	Username     string             `bson:"username"`
-	Phone        string             `bson:"phone,omitempty"`
-	PasswordHash string             `bson:"password_hash,omitempty"`
-	AvatarURL    string             `bson:"avatar_url,omitempty"`
-	Bio          string             `bson:"bio,omitempty"`
-	Birthday     time.Time          `bson:"birthday,omitempty"`
-	Roles        []string           `bson:"roles,omitempty"`
-	CreatedAt    time.Time          `bson:"created_at"`
-	UpdatedAt    time.Time          `bson:"updated_at"`
-	DeletedAt    *time.Time         `bson:"deleted_at,omitempty"`
+	ID           primitive.ObjectID   `bson:"_id,omitempty"`
+	Username     string               `bson:"username"`
+	Phone        string               `bson:"phone,omitempty"`
+	PasswordHash string               `bson:"password_hash,omitempty"`
+	AvatarURL    string               `bson:"avatar_url,omitempty"`
+	Bio          string               `bson:"bio,omitempty"`
+	Birthday     time.Time            `bson:"birthday,omitempty"`
+	Roles        []primitive.ObjectID `bson:"roles,omitempty"`
+	Permissions  []primitive.ObjectID `bson:"permissions,omitempty"`
+	CreatedAt    time.Time            `bson:"created_at"`
+	UpdatedAt    time.Time            `bson:"updated_at"`
+	DeletedAt    *time.Time           `bson:"deleted_at,omitempty"`
+}
+
+const (
+	RoleAdmin Role = "admin"
+	RoleUser  Role = "user"
+)
+
+const (
+	PermissionCreatePost Permission = "create_post"
+	PermissionUpdatePost Permission = "update_post"
+	PermissionDeletePost Permission = "delete_post"
+	PermissionReadPost   Permission = "read_post"
+)
+
+type Roles struct {
+	ID primitive.ObjectID `bson:"_id,omitempty"`
+	Name Role `bson:"name"`
+	Permissions []string `bson:"permissions"`
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
+	DeletedAt *time.Time `bson:"deleted_at,omitempty"`
+}
+
+type Permissions struct {
+	ID primitive.ObjectID `bson:"_id,omitempty"`
+	Name Permission `bson:"name"`
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
+	DeletedAt *time.Time `bson:"deleted_at,omitempty"`
 }
